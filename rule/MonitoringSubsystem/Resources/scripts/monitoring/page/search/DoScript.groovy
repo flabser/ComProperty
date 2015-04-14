c3b9f57275d9aa7b56967b74d943369ae0f05baa
Mariya <dzhillian@gmail.com>
2015-04-14 14:31:28 +0600

    changed search formula

--- rule/MonitoringSubsystem/Resources/scripts/monitoring/page/search/DoScript.groovy
+++ rule/MonitoringSubsystem/Resources/scripts/monitoring/page/search/DoScript.groovy
@@ -5,16 +5,18 @@ import kz.nextbase.script.events._DoScript
 import nextbase.groovy.*
 
 class DoScript extends _DoScript {
-	
 
-	public void doProcess(_Session session, _WebFormData formData, String lang) {
-		println(formData)
-		def page = formData.getValueSilently("page");
-		if (page ==''){
-			page=1;
-		}
-		def db = session.getCurrentDatabase()
-		def col = db.search(formData.getValue("keyword"), page as int)
-		setContent(col)
-	}
+
+    public void doProcess(_Session session, _WebFormData formData, String lang) {
+
+        def page = formData.getNumberValueSilently("page", 1);
+
+        def db = session.getCurrentDatabase()
+        def condition = "form in ('furniture', 'schoolequipment', 'buildings', 'automobile', 'cargo', 'dejtransport', 'officialtransport', 'hospitaltransport', 'bus', 'trolleybus', 'tram', 'watertransport', 'taxi', 'specialequipment', 'motorcycle', 'objectreservedfund', \n" +
+                "  'bombproof', 'factory', 'combines', 'airport', 'transitions', 'billboard', 'columns', 'electricnetworks', 'thermalnetworks', 'gas', 'watersystem', 'drain', 'road', 'parking', 'order', 'contract')  ";
+        String[] cond = ["","", "","","","", condition]
+        def col = db.search(formData.getValue("keyword"), page as int, cond)
+        //def col = db.search(formData.getValue("keyword"), page as int)
+        setContent(col)
+    }
 }
