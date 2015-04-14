a85da58e1251b697c77c058b51593ea2843947b8
Mariya <dzhillian@gmail.com>
2015-04-14 14:34:33 +0600

    changed search formula

--- rule/RegistrySubsystem/Resources/scripts/page/registry_search/DoScript.groovy
+++ rule/RegistrySubsystem/Resources/scripts/page/registry_search/DoScript.groovy
@@ -1,8 +1,6 @@
 package page.registry_search
 import kz.nextbase.script._Session
-import kz.nextbase.script._Tag
 import kz.nextbase.script._WebFormData
-import kz.nextbase.script._XMLDocument
 import kz.nextbase.script.events._DoScript
 import nextbase.groovy.*
 
@@ -12,7 +10,7 @@ class DoScript extends _DoScript {
 	public void doProcess(_Session session, _WebFormData formData, String lang) {
 		def page = formData.getNumberValueSilently("page", 1);
 		def db = session.getCurrentDatabase()
-        def condition = "form='kgu' or form='kgp' or form='gkkp' or form='ao' or form='too' or form='ao' or form='individuals' or form='legalentities'";
+        def condition = "form in ('kgu', 'kgp', 'gkkp', 'ao', 'too', 'ao', 'individuals', 'legalentities')";
 		//def col = db.search(formData.getValue("keyword"), page as int)
         def col = db.getCollectionOfDocuments("($condition) and viewtext ~* '${formData.getEncodedValueSilently("keyword")}'",
                 page, false);
