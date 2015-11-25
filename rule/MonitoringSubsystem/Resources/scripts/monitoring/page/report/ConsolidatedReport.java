2b8059c1d16b34554ffa4ecf471b0f512902364d
flabser <info@flabser.com>
2015-11-25 16:41:17 +0200

    add report

--- rule/MonitoringSubsystem/Resources/scripts/monitoring/page/report/ConsolidatedReport.java
+++ rule/MonitoringSubsystem/Resources/scripts/monitoring/page/report/ConsolidatedReport.java
@@ -3,9 +3,22 @@ package monitoring.page.report;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeSet;
 
+import kz.flabs.dataengine.DatabaseFactory;
+import kz.flabs.dataengine.IDatabase;
+import kz.flabs.dataengine.ISelectFormula;
+import kz.flabs.dataengine.h2.queryformula.SelectFormula;
+import kz.flabs.parser.FormulaBlocks;
+import kz.flabs.runtimeobj.document.DocID;
+import kz.flabs.users.RunTimeParameters;
 import kz.flabs.util.Util;
+import kz.flabs.webrule.constants.QueryType;
+import kz.nextbase.script._Document;
+import kz.nextbase.script._Exception;
 import kz.nextbase.script._Session;
+import kz.nextbase.script._ViewEntry;
+import kz.nextbase.script._ViewEntryCollection;
 import kz.nextbase.script._WebFormData;
 import kz.nextbase.script.events._DoScript;
 import kz.pchelka.server.Server;
@@ -22,27 +35,47 @@ import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 
 public class ConsolidatedReport extends _DoScript {
+	private String lang;
+	String[] personalstateCat = { "furniture", "animals", "sportsequipment", "others", "shareblocks", "equity" };
+	String[] equipmentCat = { "schoolequipment", "officeequipment", "computerequipment", "medicalequipment",
+			"cookequipment", "equipmentofcivildefense" };
+	String[] realestateCat = { "buildings", "rooms", "structures", "residentialobjects", "land", "monument" };
+	String[] transportCat = { "automobile", "cargo", "bus", "trolleybus", "tram", "watertransport", "hospitaltransport",
+			"specialequipment", "motorcycle" };
+	String[] strategicobjectsCat = { "billboard", "columns", "electricnetworks", "thermalnetworks", "gas",
+			"watersystem", "drain", "road", "parking" };
+	String[] specialconstructionsCat = { "bombproof", "factory", "combines", "airport", "land", "transitions" };
+	String[] engineeringInfrastructureCat = { "shareblocks", "equity" };
 
 	@Override
 	public void doProcess(_Session ses, _WebFormData formData, String lang) {
+		this.lang = lang;
 		println(formData);
-		String reportName = "firstreport";
+		String reportName = "consolidated_report";
+		HashMap<String, String[]> categories = new HashMap<String, String[]>();
+		categories.put("personalstateCat", personalstateCat);
+		categories.put("equipmentCat", equipmentCat);
+		categories.put("realestateCat", realestateCat);
+		categories.put("transportCat", transportCat);
+		categories.put("strategicobjectsCat", strategicobjectsCat);
+		categories.put("specialconstructionsCat", specialconstructionsCat);
+		categories.put("engineeringInfrastructureCat", engineeringInfrastructureCat);
+
 		// formData.getValue("typefilereport").equals("1")
-		int type = 1;
+		int type = 2;
 
 		HashMap<String, Object> parameters = new HashMap<String, Object>();
 
 		try {
 			log("Filling report...");
-			JRFileVirtualizer virtualizer = new JRFileVirtualizer(10,
-					new File("").getAbsolutePath() + "\\webapps\\MonitoringSubsystem\\reports");
+			String repPath = new File("").getAbsolutePath() + "\\webapps\\MonitoringSubsystem\\reports";
+			JRFileVirtualizer virtualizer = new JRFileVirtualizer(10, repPath);
 			parameters.put(JRParameter.REPORT_VIRTUALIZER, virtualizer);
 
-			JRBeanCollectionDataSource dSource = new JRBeanCollectionDataSource(fetchData());
+			JRBeanCollectionDataSource dSource = new JRBeanCollectionDataSource(fetchData(categories));
 
 			JasperPrint print = JasperFillManager.fillReport(
-					JasperCompileManager.compileReportToFile(
-							"C:\\Users\\k-zon_000\\JaspersoftWorkspace\\MyReports\\" + reportName + ".jrxml"),
+					JasperCompileManager.compileReportToFile(repPath + "\\templates\\" + reportName + ".jrxml"),
 					parameters, dSource);
 
 			if (type == 1) {
@@ -62,13 +95,46 @@ public class ConsolidatedReport extends _DoScript {
 		}
 	}
 
-	private ArrayList<ReportRowEntity> fetchData() {
+	private ArrayList<ReportRowEntity> fetchData(HashMap<String, String[]> categories) {
 		ArrayList<ReportRowEntity> data = new ArrayList<ReportRowEntity>();
+		IDatabase db = DatabaseFactory.getDatabase("MonitoringSubsystem");
+		for (String key : categories.keySet()) {
+			String[] toReport = categories.get(key);
+			for (int ci = 0; ci < toReport.length; ci++) {
+				FormulaBlocks queryFormulaBlocks = new FormulaBlocks("form=\"" + toReport[ci] + "\"",
+						QueryType.DOCUMENT);
+				ISelectFormula sf = new SelectFormula(queryFormulaBlocks);
+				_ViewEntryCollection vec = db.getCollectionByCondition(sf, ses.getUser(), 0, 0, new TreeSet<DocID>(),
+						new RunTimeParameters(), false);
+				ReportRowEntity object = new ReportRowEntity();
+				object.setCategory(getLocalizedWord(key, lang));
+				object.setSubCategory(getLocalizedWord(toReport[ci], lang));
+				object.setCount(vec.getCount());
+				int originalCostSum = 0, cumulativedepreciationSum = 0, balanceCostSum = 0;
+				for (_ViewEntry e : vec.getEntries()) {
+					try {
+						_Document doc = e.getDocument();
+						originalCostSum = originalCostSum + doc.getValueInt("originalcost");
+						cumulativedepreciationSum = cumulativedepreciationSum
+								+ doc.getValueInt("cumulativedepreciation");
+						balanceCostSum = balanceCostSum + doc.getValueInt("balancecost");
+					} catch (_Exception e1) {
+						Server.logger.errorLogEntry(e1);
+					}
 
-		int iteration = 10;
-		for (int i = 0; i < iteration; i++) {
-			data.add(generateMock());
+				}
+				object.setPrimaryCost(originalCostSum);
+				object.setDepreciation(cumulativedepreciationSum);
+				object.setBookvalue(balanceCostSum);
+				object.setReassessmentCost(0);
+				data.add(object);
+			}
 		}
+
+		/*
+		 * int iteration = 10; for (int i = 0; i < iteration; i++) {
+		 * data.add(generateMock()); }
+		 */
 
 		return data;
 
