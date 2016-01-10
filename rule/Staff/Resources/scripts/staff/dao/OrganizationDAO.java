c726b6ef27637ae4b06f472e17f762ec3fc6fe18
k-zon_000 <k-zon_000@KayraNote>
2016-01-10 20:46:57 +0200

    fix

--- rule/Staff/Resources/scripts/staff/dao/OrganizationDAO.java
+++ rule/Staff/Resources/scripts/staff/dao/OrganizationDAO.java
@@ -6,8 +6,13 @@ import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import kz.flabs.dataengine.jpa.DAO;
+import kz.flabs.runtimeobj.RuntimeObjUtil;
 import kz.nextbase.script._Session;
 import staff.model.Organization;
 
@@ -17,27 +22,30 @@ public class OrganizationDAO extends DAO<Organization, UUID> {
 		super(Organization.class, session);
 	}
 
-	public List<Organization> findAllByKeyword(String keyword, int firstRec, int pageSize) {
+	public DAO<Organization, UUID>.ViewPage<Organization> findAllByKeyword(String keyword, int pageNum, int pageSize) {
 		EntityManager em = getEntityManagerFactory().createEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
 		try {
-			String jpql = "SELECT m FROM Organization AS m WHERE m.name LIKE :keyword";
-			TypedQuery<Organization> q = em.createQuery(jpql, Organization.class);
-			q.setFirstResult(firstRec);
-			q.setMaxResults(pageSize);
-			q.setParameter("keyword", "%" + keyword + "%");
-			return q.getResultList();
-		} finally {
-			em.close();
-		}
-	}
-
-	public Long getCountByKeyword(String keyword) {
-		EntityManager em = getEntityManagerFactory().createEntityManager();
-		try {
-			String jpql = "SELECT count(m) FROM Organization AS m WHERE m.name LIKE :keyword";
-			Query q = em.createQuery(jpql);
-			q.setParameter("keyword", "%" + keyword + "%");
-			return (Long) q.getSingleResult();
+			CriteriaQuery<Organization> cq = cb.createQuery(getEntityClass());
+			CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
+			Root<Organization> c = cq.from(getEntityClass());
+			cq.select(c);
+			countCq.select(cb.count(c));
+			Predicate condition = cb.like(cb.lower(c.get("name")), "%" + keyword.toLowerCase() + "%");
+			cq.where(condition);
+			countCq.where(condition);
+			TypedQuery<Organization> typedQuery = em.createQuery(cq);
+			Query query = em.createQuery(countCq);
+			long count = (long) query.getSingleResult();
+			int maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
+			if (pageNum == 0) {
+				pageNum = maxPage;
+			}
+			int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
+			typedQuery.setFirstResult(firstRec);
+			typedQuery.setMaxResults(pageSize);
+			List<Organization> result = typedQuery.getResultList();
+			return new ViewPage<Organization>(result, count, maxPage, pageNum);
 		} finally {
 			em.close();
 		}
