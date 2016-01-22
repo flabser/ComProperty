7a550da5566fb7b04099608481783af6980aa1ac
medin84 <kalihanovm@gmail.com>
2016-01-22 19:19:48 +0600

    commit

--- rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/AirportView.java
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/AirportView.java
@@ -5,22 +5,19 @@ import kz.nextbase.script._WebFormData;
 import municipalproperty.model.constants.KufType;
 
 /**
- * 
- * 
  * @author Kayra created 09-01-2016
  */
 
 public class AirportView extends MunicipalPropertyView {
 
-	@Override
-	public void doGET(_Session session, _WebFormData formData, String lang) {
-		setContent(getSimpleActionBar(session, "airport", lang));
-		setContent(getPropertyViewPage(session, formData, KufType.AIRPORT));
-	}
+    @Override
+    public void doGET(_Session session, _WebFormData formData, String lang) {
+        setContent(getSimpleActionBar(session, "airport-form", lang));
+        setContent(getPropertyViewPage(session, formData, KufType.AIRPORT));
+    }
 
-	@Override
-	public void doPOST(_Session session, _WebFormData formData, String lang) {
+    @Override
+    public void doPOST(_Session session, _WebFormData formData, String lang) {
 
-	}
-
+    }
 }
--- rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/FurnitureView.java
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/FurnitureView.java
@@ -4,18 +4,17 @@ import kz.nextbase.script._Session;
 import kz.nextbase.script._WebFormData;
 import municipalproperty.model.constants.KufType;
 
+
 public class FurnitureView extends MunicipalPropertyView {
 
-	@Override
-	public void doGET(_Session session, _WebFormData formData, String lang) {
-		// println(formData);
-		setContent(getSimpleActionBar(session, "furniture", lang));
-		setContent(getPropertyViewPage(session, formData, KufType.FURNITURE));
-	}
+    @Override
+    public void doGET(_Session session, _WebFormData formData, String lang) {
+        setContent(getSimpleActionBar(session, "furniture-form", lang));
+        setContent(getPropertyViewPage(session, formData, KufType.FURNITURE));
+    }
 
-	@Override
-	public void doPOST(_Session session, _WebFormData formData, String lang) {
+    @Override
+    public void doPOST(_Session session, _WebFormData formData, String lang) {
 
-	}
-
+    }
 }
