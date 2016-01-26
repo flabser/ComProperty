8aadcd2707867d4a7bfdc3cdd5ddefc474e992e7
k-zon_000 <k-zon_000@KayraNote>
2016-01-26 15:37:40 +0200

    add DAO

--- rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/FurnitureView.java
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/FurnitureView.java
@@ -4,17 +4,16 @@ import kz.nextbase.script._Session;
 import kz.nextbase.script._WebFormData;
 import municipalproperty.model.constants.KufType;
 
-
 public class FurnitureView extends MunicipalPropertyView {
 
-    @Override
-    public void doGET(_Session session, _WebFormData formData, String lang) {
-        setContent(getSimpleActionBar(session, "furniture-form", lang));
-        setContent(getPropertyViewPage(session, formData, KufType.FURNITURE));
-    }
+	@Override
+	public void doGET(_Session session, _WebFormData formData, String lang) {
+		setContent(getSimpleActionBar(session, "furniture-form", lang));
+		setContent(getPropertyViewPage(session, formData, KufType.FURNITURE));
+	}
 
-    @Override
-    public void doPOST(_Session session, _WebFormData formData, String lang) {
+	@Override
+	public void doPOST(_Session session, _WebFormData formData, String lang) {
 
-    }
+	}
 }
