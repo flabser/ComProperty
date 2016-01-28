6f75f84df63fb313e131d109051ff63ec401e859
k-zon_000 <k-zon_000@KayraNote>
2016-01-28 09:22:47 +0200

    deleted unused xsl

--- rule/MunicipalProperty/Resources/scripts/municipalproperty/page/action/WrittenOff.java
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/page/action/WrittenOff.java
@@ -1,8 +1,14 @@
 package municipalproperty.page.action;
 
+import java.util.UUID;
+
+import kz.nextbase.script._Exception;
 import kz.nextbase.script._Session;
 import kz.nextbase.script._WebFormData;
 import kz.nextbase.script.events._DoPage;
+import municipalproperty.dao.PropertyDAO;
+import municipalproperty.model.Property;
+import municipalproperty.model.constants.StatusType;
 
 public class WrittenOff extends _DoPage {
 
@@ -12,7 +18,15 @@ public class WrittenOff extends _DoPage {
 	}
 
 	@Override
-	public void doPOST(_Session session, _WebFormData webFormData, String lang) {
-
+	public void doPOST(_Session session, _WebFormData webFormData, String lang) throws _Exception {
+		String id = webFormData.getValue("docid");
+		PropertyDAO dao = new PropertyDAO(ses);
+		Property entity = dao.findById(UUID.fromString(id));
+		if (entity != null) {
+			entity.setStatus(StatusType.WRITTENOFF);
+			dao.update(entity);
+		} else {
+			setBadRequest();
+		}
 	}
 }
