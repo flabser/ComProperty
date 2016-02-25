2350c1039831a10a17864ebec0ae65fd5b3e1718
medin84 <kalihanovm@gmail.com>
2016-02-25 20:16:18 +0600

    commit

--- rule/Reference/Resources/scripts/reference/page/view/CountryView.java
+++ rule/Reference/Resources/scripts/reference/page/view/CountryView.java
@@ -8,23 +8,33 @@ import kz.nextbase.script.actions._ActionBar;
 import kz.nextbase.script.actions._ActionType;
 import kz.nextbase.script.events._DoPage;
 import reference.dao.CountryDAO;
+import reference.model.Country;
+
+import java.util.UUID;
+
 
 public class CountryView extends _DoPage {
 
-	@Override
-	public void doGET(_Session session, _WebFormData formData, LanguageType lang) {
-		_ActionBar actionBar = new _ActionBar(session);
-		_Action newDocAction = new _Action(getLocalizedWord("new_", lang), "", "new_country");
-		newDocAction.setURL("Provider?id=country-form");
-		actionBar.addAction(newDocAction);
-		actionBar.addAction(new _Action(getLocalizedWord("del_document", lang), "", _ActionType.DELETE_DOCUMENT));
+    @Override
+    public void doGET(_Session session, _WebFormData formData, LanguageType lang) {
+        _ActionBar actionBar = new _ActionBar(session);
+        _Action newDocAction = new _Action(getLocalizedWord("new_", lang), "", "new_country");
+        newDocAction.setURL("Provider?id=country-form");
+        actionBar.addAction(newDocAction);
+        actionBar.addAction(new _Action(getLocalizedWord("del_document", lang), "", _ActionType.DELETE_DOCUMENT));
 
-		addContent(actionBar);
-		addContent(getViewPage(new CountryDAO(session), formData));
-	}
+        addContent(actionBar);
+        addContent(getViewPage(new CountryDAO(session), formData));
+    }
 
-	@Override
-	public void doPOST(_Session session, _WebFormData formData, LanguageType lang) {
+    @Override
+    public void doDELETE(_Session session, _WebFormData formData, LanguageType lang) {
+        println(formData);
 
-	}
+        CountryDAO dao = new CountryDAO(session);
+        for (String id : formData.getListOfValuesSilently("docid")) {
+            Country c = dao.findById(UUID.fromString(id));
+            dao.delete(c);
+        }
+    }
 }
