f29948594a5fe8cb5683774e8af234974fdd722a
medin84 <kalihanovm@gmail.com>
2016-02-26 22:35:01 +0600

    commit

--- rule/Staff/Resources/scripts/staff/page/action/GetOrganizationsAction.java
+++ rule/Staff/Resources/scripts/staff/page/action/GetOrganizationsAction.java
@@ -10,30 +10,18 @@ import staff.dao.OrganizationDAO;
 import staff.model.Organization;
 
 /**
- * 
- * 
  * @author Kayra created 09-01-2016
  */
 
 public class GetOrganizationsAction extends _DoPage {
 
-	@Override
-	public void doGET(_Session ses, _WebFormData formData, LanguageType lang) {
-		String keyword = formData.getValueSilently("keyword");
-		int pageNum = 1;
-		int pageSize = ses.pageSize;
-		if (formData.containsField("page")) {
-			pageNum = formData.getNumberValueSilently("page", pageNum);
-		}
-		OrganizationDAO dao = new OrganizationDAO(ses);
-		ViewPage<Organization> vp = dao.findAllByKeyword(keyword, pageNum, pageSize);
-		addContent(new _POJOListWrapper(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum(), lang));
-		// setPublishAsType(PublishAsType.JSON);
-	}
-
-	@Override
-	public void doPOST(_Session session, _WebFormData webFormData, LanguageType lang) {
-
-	}
-
+    @Override
+    public void doGET(_Session ses, _WebFormData formData, LanguageType lang) {
+        String keyword = formData.getValueSilently("keyword");
+        int pageNum = formData.getNumberValueSilently("page", 1);
+        int pageSize = ses.pageSize;
+        OrganizationDAO dao = new OrganizationDAO(ses);
+        ViewPage<Organization> vp = dao.findAllByKeyword(keyword, pageNum, pageSize);
+        addContent(new _POJOListWrapper(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum(), lang));
+    }
 }
