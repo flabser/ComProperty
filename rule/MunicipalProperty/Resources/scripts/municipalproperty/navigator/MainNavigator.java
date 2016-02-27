43b874c701e9a0ffee207eb93c02dc1312738b95
medin84 <kalihanovm@gmail.com>
2016-02-27 23:39:59 +0600

    commit

--- rule/MunicipalProperty/Resources/scripts/municipalproperty/navigator/MainNavigator.java
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/navigator/MainNavigator.java
@@ -1,17 +1,15 @@
 package municipalproperty.navigator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import kz.flabs.localization.LanguageType;
 import kz.lof.scripting._Session;
 import kz.lof.webserver.servlet.IOutcomeObject;
-import kz.nextbase.script._Tag;
 import kz.nextbase.script._WebFormData;
-import kz.nextbase.script._XMLDocument;
 import kz.nextbase.script.events._DoPage;
 import kz.nextbase.script.outline._Outline;
 import kz.nextbase.script.outline._OutlineEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Kayra created 24-12-2015
@@ -19,106 +17,98 @@ import kz.nextbase.script.outline._OutlineEntry;
 
 public class MainNavigator extends _DoPage {
 
-	@Override
-	public void doGET(_Session session, _WebFormData formData, LanguageType lang) {
-		List<IOutcomeObject> list = new ArrayList<IOutcomeObject>();
+    @Override
+    public void doGET(_Session session, _WebFormData formData, LanguageType lang) {
+        List<IOutcomeObject> list = new ArrayList<IOutcomeObject>();
 
-		_Outline munPropOutline = new _Outline(getLocalizedWord("municipal_property", lang), "municipal_property");
+        _Outline munPropOutline = new _Outline(getLocalizedWord("municipal_property", lang), "municipal_property");
 
-		// if(cuser.hasRole(["struct_keeper", "supervisor"])) {
-		_OutlineEntry personalEstateEntry = new _OutlineEntry(getLocalizedWord("personal_estates", lang), "personal-estate-view");
-		personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("furnitures", lang), "furniture-view"));
-		personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("animals", lang), "animal-view"));
-		personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("inventory", lang), "inventory-view"));
-		personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("others", lang), "others-view"));
+        // if(cuser.hasRole(["struct_keeper", "supervisor"])) {
+        _OutlineEntry personalEstateEntry = new _OutlineEntry(getLocalizedWord("personal_estates", lang), "personal-estate-view");
+        personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("furnitures", lang), "furniture-view"));
+        personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("animals", lang), "animal-view"));
+        personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("inventory", lang), "inventory-view"));
+        personalEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("others", lang), "others-view"));
 
-		_OutlineEntry intangibleAssetsEntry = new _OutlineEntry(getLocalizedWord("intangible_assets", lang), "intangible-asset-view");
-		intangibleAssetsEntry.addEntry(new _OutlineEntry(getLocalizedWord("share_blocks", lang), "share-block-view"));
-		intangibleAssetsEntry.addEntry(new _OutlineEntry(getLocalizedWord("equities", lang), "equity-view"));
+        _OutlineEntry intangibleAssetsEntry = new _OutlineEntry(getLocalizedWord("intangible_assets", lang), "intangible-asset-view");
+        intangibleAssetsEntry.addEntry(new _OutlineEntry(getLocalizedWord("share_blocks", lang), "share-block-view"));
+        intangibleAssetsEntry.addEntry(new _OutlineEntry(getLocalizedWord("equities", lang), "equity-view"));
 
-		_OutlineEntry equipmentEntry = new _OutlineEntry(getLocalizedWord("equipment", lang), "equipment-view");
-		equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("school_equipment", lang), "school-equipment-view"));
-		equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("office_equipment", lang), "office-equipment-view"));
-		equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("computer_equipment", lang), "computer-equipment-view"));
-		equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("medical_equipment", lang), "medical-equipment-view"));
-		equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("cook_equipment", lang), "cook-equipment-view"));
-		equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("equipment_of_civildefence", lang), "equipment-of-civil-defence-view"));
-		equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("others", lang), "others-equipment-view"));
+        _OutlineEntry equipmentEntry = new _OutlineEntry(getLocalizedWord("equipment", lang), "equipment-view");
+        equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("school_equipment", lang), "school-equipment-view"));
+        equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("office_equipment", lang), "office-equipment-view"));
+        equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("computer_equipment", lang), "computer-equipment-view"));
+        equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("medical_equipment", lang), "medical-equipment-view"));
+        equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("cook_equipment", lang), "cook-equipment-view"));
+        equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("equipment_of_civildefence", lang), "equipment-of-civil-defence-view"));
+        equipmentEntry.addEntry(new _OutlineEntry(getLocalizedWord("others", lang), "others-equipment-view"));
 
-		_OutlineEntry realEstateEntry = new _OutlineEntry(getLocalizedWord("real_estates", lang), "real-estate-view");
-		realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("buildings", lang), "buildings-view"));
-		realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("structures", lang), "structures-view"));
-		realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("residential_objects", lang), "residential-objects-view"));
-		realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("land", lang), "land-view"));
-		realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("monuments", lang), "monument-view"));
+        _OutlineEntry realEstateEntry = new _OutlineEntry(getLocalizedWord("real_estates", lang), "real-estate-view");
+        realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("buildings", lang), "buildings-view"));
+        realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("structures", lang), "structures-view"));
+        realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("residential_objects", lang), "residential-objects-view"));
+        realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("land", lang), "land-view"));
+        realEstateEntry.addEntry(new _OutlineEntry(getLocalizedWord("monuments", lang), "monument-view"));
 
-		_OutlineEntry transportEntry = new _OutlineEntry(getLocalizedWord("transport", lang), "transport-view");
-		_OutlineEntry autoEntry = new _OutlineEntry(getLocalizedWord("automobiles", lang), "automobile-view");
-		transportEntry.addEntry(autoEntry);
-		autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("cars", lang), "car-view"));
-		autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("cargo", lang), "cargo-view"));
-		autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("dej_transport", lang), "dej-transport-view"));
-		autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("official_transport", lang), "official-transport-view"));
-		_OutlineEntry passengerTransportEntry = new _OutlineEntry(getLocalizedWord("passenger_transport", lang), "passenger-transport-view");
-		passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("buses", lang), "bus-view"));
-		passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("trolleybuses", lang), "trolleybus-view"));
-		passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("trams", lang), "tram-view"));
-		passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("water_transports", lang), "water-transport-view"));
-		passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("taxi", lang), "taxi-view"));
-		autoEntry.addEntry(passengerTransportEntry);
-		transportEntry.addEntry(new _OutlineEntry(getLocalizedWord("special_equipment", lang), "special-equipment-view"));
-		transportEntry.addEntry(new _OutlineEntry(getLocalizedWord("motorcycles", lang), "motorcycle-view"));
+        _OutlineEntry transportEntry = new _OutlineEntry(getLocalizedWord("transport", lang), "transport-view");
+        _OutlineEntry autoEntry = new _OutlineEntry(getLocalizedWord("automobiles", lang), "automobile-view");
+        transportEntry.addEntry(autoEntry);
+        autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("cars", lang), "car-view"));
+        autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("cargo", lang), "cargo-view"));
+        autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("dej_transport", lang), "dej-transport-view"));
+        autoEntry.addEntry(new _OutlineEntry(getLocalizedWord("official_transport", lang), "official-transport-view"));
+        _OutlineEntry passengerTransportEntry = new _OutlineEntry(getLocalizedWord("passenger_transport", lang), "passenger-transport-view");
+        passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("buses", lang), "bus-view"));
+        passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("trolleybuses", lang), "trolleybus-view"));
+        passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("trams", lang), "tram-view"));
+        passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("water_transports", lang), "water-transport-view"));
+        passengerTransportEntry.addEntry(new _OutlineEntry(getLocalizedWord("taxi", lang), "taxi-view"));
+        autoEntry.addEntry(passengerTransportEntry);
+        transportEntry.addEntry(new _OutlineEntry(getLocalizedWord("special_equipment", lang), "special-equipment-view"));
+        transportEntry.addEntry(new _OutlineEntry(getLocalizedWord("motorcycles", lang), "motorcycle-view"));
 
-		_OutlineEntry strategicObjectEntry = new _OutlineEntry(getLocalizedWord("strategic_objects", lang), "strategic-object-view");
-		strategicObjectEntry.addEntry(new _OutlineEntry(getLocalizedWord("object_reserved_fund", lang), "object-reserved-fund-view"));
-		_OutlineEntry specialConstructionEntry = new _OutlineEntry(getLocalizedWord("special_constructions", lang), "special-construction-view");
-		specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("bombproofs", lang), "bomb-proof-view"));
-		specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("factory", lang), "factory-view"));
-		specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("combines", lang), "combine-view"));
-		specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("airports", lang), "airport-view"));
-		specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("transitions", lang), "transition-view"));
-		strategicObjectEntry.addEntry(specialConstructionEntry);
+        _OutlineEntry strategicObjectEntry = new _OutlineEntry(getLocalizedWord("strategic_objects", lang), "strategic-object-view");
+        strategicObjectEntry.addEntry(new _OutlineEntry(getLocalizedWord("object_reserved_fund", lang), "object-reserved-fund-view"));
+        _OutlineEntry specialConstructionEntry = new _OutlineEntry(getLocalizedWord("special_constructions", lang), "special-construction-view");
+        specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("bombproofs", lang), "bomb-proof-view"));
+        specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("factory", lang), "factory-view"));
+        specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("combines", lang), "combine-view"));
+        specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("airports", lang), "airport-view"));
+        specialConstructionEntry.addEntry(new _OutlineEntry(getLocalizedWord("transitions", lang), "transition-view"));
+        strategicObjectEntry.addEntry(specialConstructionEntry);
 
-		_OutlineEntry engStructureEntry = new _OutlineEntry(getLocalizedWord("eng_infrastructure", lang), "engineering-infrastructure-view");
-		engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("billboards", lang), "billboard-view"));
-		engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("columns", lang), "column-view"));
-		_OutlineEntry networkEntry = new _OutlineEntry(getLocalizedWord("networks", lang), "network-view");
-		engStructureEntry.addEntry(networkEntry);
-		networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("electric_networks", lang), "electric-network-view"));
-		networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("thermal_networks", lang), "thermal-network-view"));
-		networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("gas", lang), "gas-view"));
-		networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("water_systems", lang), "water-system-view"));
-		networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("drain", lang), "drain-view"));
-		engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("roads", lang), "road-view"));
-		engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("parkings", lang), "parking-view"));
+        _OutlineEntry engStructureEntry = new _OutlineEntry(getLocalizedWord("eng_infrastructure", lang), "engineering-infrastructure-view");
+        engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("billboards", lang), "billboard-view"));
+        engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("columns", lang), "column-view"));
+        _OutlineEntry networkEntry = new _OutlineEntry(getLocalizedWord("networks", lang), "network-view");
+        engStructureEntry.addEntry(networkEntry);
+        networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("electric_networks", lang), "electric-network-view"));
+        networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("thermal_networks", lang), "thermal-network-view"));
+        networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("gas", lang), "gas-view"));
+        networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("water_systems", lang), "water-system-view"));
+        networkEntry.addEntry(new _OutlineEntry(getLocalizedWord("drain", lang), "drain-view"));
+        engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("roads", lang), "road-view"));
+        engStructureEntry.addEntry(new _OutlineEntry(getLocalizedWord("parkings", lang), "parking-view"));
 
-		munPropOutline.addEntry(personalEstateEntry);
-		munPropOutline.addEntry(intangibleAssetsEntry);
-		munPropOutline.addEntry(equipmentEntry);
-		munPropOutline.addEntry(realEstateEntry);
-		munPropOutline.addEntry(transportEntry);
-		munPropOutline.addEntry(strategicObjectEntry);
-		munPropOutline.addEntry(engStructureEntry);
+        munPropOutline.addEntry(personalEstateEntry);
+        munPropOutline.addEntry(intangibleAssetsEntry);
+        munPropOutline.addEntry(equipmentEntry);
+        munPropOutline.addEntry(realEstateEntry);
+        munPropOutline.addEntry(transportEntry);
+        munPropOutline.addEntry(strategicObjectEntry);
+        munPropOutline.addEntry(engStructureEntry);
 
-		_Outline orderOutline = new _Outline(getLocalizedWord("orders", lang), "order");
-		orderOutline.addEntry(new _OutlineEntry(getLocalizedWord("orders", lang), "order-view"));
+        _Outline orderOutline = new _Outline(getLocalizedWord("orders", lang), "order");
+        orderOutline.addEntry(new _OutlineEntry(getLocalizedWord("orders", lang), "order-view"));
 
-		_Outline reportOutline = new _Outline(getLocalizedWord("reports", lang), "report");
-		reportOutline.addEntry(new _OutlineEntry(getLocalizedWord("reports", lang), "report-template-view"));
+        _Outline reportOutline = new _Outline(getLocalizedWord("reports", lang), "report");
+        reportOutline.addEntry(new _OutlineEntry(getLocalizedWord("reports", lang), "report-template-view"));
 
-		list.add(munPropOutline);
-		list.add(orderOutline);
-		list.add(reportOutline);
+        list.add(munPropOutline);
+        list.add(orderOutline);
+        list.add(reportOutline);
 
-		_Tag currentTag = new _Tag("current");
-		currentTag.setAttr("id", formData.getValueSilently("id").replace("-form", "-view"));
-
-		addContent(new _XMLDocument(currentTag));
-		addContent(list);
-	}
-
-	@Override
-	public void doPOST(_Session session, _WebFormData formData, LanguageType lang) {
-
-	}
+        addContent("outline_current", formData.getValueSilently("id").replace("-form", "-view"));
+        addContent(list);
+    }
 }
--- rule/Reference/Resources/scripts/reference/navigator/MainNavigator.java
+++ rule/Reference/Resources/scripts/reference/navigator/MainNavigator.java
@@ -1,55 +1,46 @@
 package reference.navigator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import kz.flabs.localization.LanguageType;
 import kz.lof.scripting._Session;
 import kz.lof.webserver.servlet.IOutcomeObject;
-import kz.nextbase.script._Tag;
 import kz.nextbase.script._WebFormData;
-import kz.nextbase.script._XMLDocument;
 import kz.nextbase.script.events._DoPage;
 import kz.nextbase.script.outline._Outline;
 import kz.nextbase.script.outline._OutlineEntry;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MainNavigator extends _DoPage {
 
-	@Override
-	public void doGET(_Session session, _WebFormData formData, LanguageType lang) {
-		List<IOutcomeObject> list = new ArrayList<IOutcomeObject>();
+    @Override
+    public void doGET(_Session session, _WebFormData formData, LanguageType lang) {
+        List<IOutcomeObject> list = new ArrayList<IOutcomeObject>();
 
-		_Tag currentTag = new _Tag("current");
-		currentTag.setAttr("id", formData.getValueSilently("id").replace("-form", "-view"));
+        _Outline common_outline = new _Outline(getLocalizedWord("common_reference_data", lang), "common");
 
-		_Outline common_outline = new _Outline(getLocalizedWord("common_reference_data", lang), "common");
+        // if(cuser.hasRole(["struct_keeper", "supervisor"])) {
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("countries", lang), "country-view"));
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("regions", lang), "region-view"));
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("districts", lang), "district-view"));
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("localities", lang), "locality-view"));
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("streets", lang), "street-view"));
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("org_categories", lang), "orgcategory-view"));
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("positions", lang), "position-view"));
+        common_outline.addEntry(new _OutlineEntry(getLocalizedWord("tags", lang), "tag-view"));
 
-		// if(cuser.hasRole(["struct_keeper", "supervisor"])) {
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("countries", lang), "country-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("regions", lang), "region-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("districts", lang), "district-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("localities", lang), "locality-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("streets", lang), "street-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("org_categories", lang), "orgcategory-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("positions", lang), "position-view"));
-		common_outline.addEntry(new _OutlineEntry(getLocalizedWord("tags", lang), "tag-view"));
+        _Outline specific_outline = new _Outline(getLocalizedWord("specific_reference_data", lang), "specific");
+        specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("kuf", lang), "kuf-view"));
+        specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("property_codes", lang), "propertycode-view"));
+        specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("receiving_reason", lang), "receivingreason-view"));
+        specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("structure_type", lang), "structuretype-view"));
+        specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("building_materials", lang), "buildingmaterial-view"));
 
-		_Outline specific_outline = new _Outline(getLocalizedWord("specific_reference_data", lang), "specific");
-		specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("kuf", lang), "kuf-view"));
-		specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("property_codes", lang), "propertycode-view"));
-		specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("receiving_reason", lang), "receivingreason-view"));
-		specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("structure_type", lang), "structuretype-view"));
-		specific_outline.addEntry(new _OutlineEntry(getLocalizedWord("building_materials", lang), "buildingmaterial-view"));
+        list.add(common_outline);
+        list.add(specific_outline);
 
-		list.add(common_outline);
-		list.add(specific_outline);
-
-		addContent(new _XMLDocument(currentTag));
-		addContent(list);
-	}
-
-	@Override
-	public void doPOST(_Session session, _WebFormData formData, LanguageType lang) {
-
-	}
+        addContent("outline_current", formData.getValueSilently("id").replace("-form", "-view"));
+        addContent(list);
+    }
 }
