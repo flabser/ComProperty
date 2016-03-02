685a23739de39ccc92079051a23b340d1305e1eb
medin84 <kalihanovm@gmail.com>
2016-03-02 17:44:52 +0600

    commit

--- rule/Staff/Resources/scripts/staff/page/action/GetEmployeesAction.java
+++ rule/Staff/Resources/scripts/staff/page/action/GetEmployeesAction.java
@@ -9,30 +9,18 @@ import kz.nextbase.script.events._DoPage;
 import staff.dao.EmployeeDAO;
 
 /**
- * 
- * 
  * @author Kayra created 09-01-2016
  */
 
 public class GetEmployeesAction extends _DoPage {
 
-	@Override
-	public void doGET(_Session ses, _WebFormData formData) {
-		String keyword = formData.getEncodedValueSilently("keyword");
-		int pageNum = 1;
-		int pageSize = ses.pageSize;
-		if (formData.containsField("page")) {
-			pageNum = formData.getNumberValueSilently("page", pageNum);
-		}
-
-		EmployeeDAO empDao = new EmployeeDAO(ses);
-		ViewPage emps = empDao.findAllByName(keyword, pageNum, pageSize);
-		addContent(new _POJOListWrapper(emps.getResult(), emps.getMaxPage(), emps.getCount(), emps.getPageNum(), ses));
-	}
-
-	@Override
-	public void doPOST(_Session session, _WebFormData webFormData) {
-
-	}
-
+    @Override
+    public void doGET(_Session ses, _WebFormData formData) {
+        String keyword = formData.getEncodedValueSilently("keyword");
+        int pageNum = formData.getNumberValueSilently("page", 1);
+        int pageSize = ses.pageSize;
+        EmployeeDAO empDao = new EmployeeDAO(ses);
+        ViewPage emps = empDao.findAllByName(keyword, pageNum, pageSize);
+        addContent(new _POJOListWrapper(emps.getResult(), emps.getMaxPage(), emps.getCount(), emps.getPageNum(), ses));
+    }
 }
