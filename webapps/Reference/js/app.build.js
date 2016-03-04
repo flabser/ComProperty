60b7907d43f4dc4f84519a32cc39ac5c6302e08c
medin84 <kalihanovm@gmail.com>
2016-03-05 00:25:44 +0600

    сделал централизованную задачу сборки с gulp, смотреть SharedResources/nb. из модулей gulp убрал

--- webapps/Reference/js/app.build.js
+++ webapps/Reference/js/app.build.js
@@ -1238,6 +1238,82 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ ])
 });
 ;
+/* Kazakh (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Dmitriy Karasyov (dmitriy.karasyov@gmail.com). */
+( function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}( function( datepicker ) {
+
+datepicker.regional.kk = {
+	closeText: "Жабу",
+	prevText: "&#x3C;Алдыңғы",
+	nextText: "Келесі&#x3E;",
+	currentText: "Бүгін",
+	monthNames: [ "Қаңтар","Ақпан","Наурыз","Сәуір","Мамыр","Маусым",
+	"Шілде","Тамыз","Қыркүйек","Қазан","Қараша","Желтоқсан" ],
+	monthNamesShort: [ "Қаң","Ақп","Нау","Сәу","Мам","Мау",
+	"Шіл","Там","Қыр","Қаз","Қар","Жел" ],
+	dayNames: [ "Жексенбі","Дүйсенбі","Сейсенбі","Сәрсенбі","Бейсенбі","Жұма","Сенбі" ],
+	dayNamesShort: [ "жкс","дсн","ссн","срс","бсн","жма","снб" ],
+	dayNamesMin: [ "Жк","Дс","Сс","Ср","Бс","Жм","Сн" ],
+	weekHeader: "Не",
+	dateFormat: "dd.mm.yy",
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional.kk );
+
+return datepicker.regional.kk;
+
+} ) );
+
+/* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Andrew Stromnov (stromnov@gmail.com). */
+( function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}( function( datepicker ) {
+
+datepicker.regional.ru = {
+	closeText: "Закрыть",
+	prevText: "&#x3C;Пред",
+	nextText: "След&#x3E;",
+	currentText: "Сегодня",
+	monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+	"Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+	monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+	"Июл","Авг","Сен","Окт","Ноя","Дек" ],
+	dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+	dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+	dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+	weekHeader: "Нед",
+	dateFormat: "dd.mm.yy",
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional.ru );
+
+return datepicker.regional.ru;
+
+} ) );
+
 /**
  * @author Medet
  */
--- webapps/SharedResources/nb/Gulpfile.js
+++ webapps/SharedResources/nb/Gulpfile.js
@@ -8,8 +8,71 @@ var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 var handlebars = require('gulp-handlebars');
 
+// npm remove gulp gulp-concat gulp-csso gulp-jshint gulp-rename gulp-uglify jshint
+
+var modules = ['Accountant', 'Staff', 'Reference', 'MunicipalProperty'];
+var _styles = {};
+var _templates = {};
+var _scripts = {};
+var upPath = '../../';
+
+// create module task
+for (var i = 0; i < modules.length; i++) {
+    var module = modules[i];
+    // _styles
+    _styles[module] = ['../css/normalize.css',
+        'css/nb.min.css',
+        upPath + module + '/css/**/*.css',
+        '!' + upPath + module + '/css/*.min.css'
+    ];
+    // _templates
+    _templates[module] = [upPath + module + '/js/templates/*.hbs'];
+    // _scripts
+    _scripts[module] = ['js/nb.build.js',
+        upPath + module + '/js/**/*.js',
+        '!' + upPath + module + '/js/*.build.js',
+        '!' + upPath + module + '/js/*.min.js'
+    ];
+
+    console.log('task', module, '\n_styles:\n', _styles[module], '\n_templates:\n', _templates[module], '\n_scripts:\n', _scripts[module]);
+
+    (function() {
+        var m = module;
+        gulp.task(m + '_styles', function() {
+            gulp.src(_styles[m])
+                .pipe(concat('all.min.css'))
+                .pipe(csso())
+                .pipe(gulp.dest(upPath + m + '/css'));
+        });
+
+        gulp.task(m + '_templates', function() {
+            gulp.src(_templates[m])
+                .pipe(handlebars({
+                    handlebars: require('handlebars')
+                }))
+                .pipe(wrap('Handlebars.template(<%= contents %>)'))
+                .pipe(declare({
+                    namespace: 'nb.templates',
+                    noRedeclare: true,
+                }))
+                .pipe(concat('templates.js'))
+                .pipe(gulp.dest(upPath + m + '/js/templates/compiled'));
+        });
+
+        gulp.task(m + '_scripts', function() {
+            gulp.src(_scripts[m])
+                .pipe(concat('app.build.js'))
+                .pipe(gulp.dest(upPath + m + '/js'))
+                .pipe(rename('app.min.js'))
+                .pipe(uglify())
+                .pipe(gulp.dest(upPath + m + '/js'));
+        });
+    })();
+}
+
+// nb task
 var js_files = ['../vendor/handlebars/handlebars.runtime-v4.0.5.js',
-    '../vendor/jquery/ui-i18n/*.js'
+    '../vendor/jquery/ui-i18n/*.js',
     'js/src/nb.js',
     'js/src/**/*.js'
 ];
@@ -56,6 +119,10 @@ gulp.task('styles', function() {
 
 gulp.task('default', ['styles', 'templates', 'lint', 'scripts'], function() {
 
+    gulp.watch(css_files, function(event) {
+        gulp.run('styles');
+    });
+
     gulp.watch(hbs_templates, function(event) {
         gulp.run('templates');
     });
@@ -64,7 +131,24 @@ gulp.task('default', ['styles', 'templates', 'lint', 'scripts'], function() {
         gulp.run('scripts');
     });
 
-    gulp.watch(css_files, function(event) {
-        gulp.run('styles');
-    });
+    // create module watch
+    for (var i = 0; i < modules.length; i++) {
+        var module = modules[i];
+
+        console.log('watch', module, '\n_styles:\n', _styles[module], '\n_templates:\n', _templates[module], '\n_scripts:\n', _scripts[module]);
+        console.log('---------------------------------');
+
+        (function() {
+            var m = module;
+            gulp.watch(_styles[m], function(event) {
+                gulp.run(m + '_styles');
+            });
+            gulp.watch(_templates[m], function(event) {
+                gulp.run(m + '_templates');
+            });
+            gulp.watch(_scripts[m], function(event) {
+                gulp.run(m + '_scripts');
+            });
+        })();
+    }
 });
