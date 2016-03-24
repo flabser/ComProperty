f28cf31eccd9e4af98e1bad82909103504217e64
k-zon_000 <k-zon_000@KayraNote>
2016-03-24 09:17:42 +0200

    RegForm

---
+++ rule/Workspace/Resources/scripts/workspace/page/form/RegForm.java
@@ -0,0 +1,52 @@
+package workspace.page.form;
+
+import kz.lof.localization.LanguageCode;
+import kz.lof.scripting._Session;
+import kz.lof.scripting._Validation;
+import kz.lof.scripting._WebFormData;
+import kz.lof.scripting.event._DoPage;
+import reference.model.constants.CountryCode;
+
+public class RegForm extends _DoPage {
+
+	@Override
+	public void doGET(_Session session, _WebFormData formData) {
+
+	}
+
+	@Override
+	public void doPOST(_Session session, _WebFormData formData) {
+		try {
+			_Validation ve = validate(formData, session.getLang());
+			if (ve.hasError()) {
+				setBadRequest();
+				setValidation(ve);
+				return;
+			}
+
+			String id = formData.getValueSilently("fio");
+			String org = formData.getValueSilently("org");
+			String orgbin = formData.getValueSilently("orgbin");
+			String login = formData.getValueSilently("login");
+			String comment = formData.getValueSilently("comment");
+
+		} catch (Exception e) {
+			error(e);
+		}
+	}
+
+	protected _Validation validate(_WebFormData formData, LanguageCode lang) {
+		_Validation ve = new _Validation();
+		if (formData.getValueSilently("name").isEmpty()) {
+			ve.addError("name", "required", getLocalizedWord("field_is_empty", lang));
+		}
+
+		if (formData.getValueSilently("code").isEmpty()) {
+			ve.addError("code", "required", getLocalizedWord("field_is_empty", lang));
+		} else if (formData.getValueSilently("code").equalsIgnoreCase(CountryCode.UNKNOWN.name())) {
+			ve.addError("code", "ne_unknown", getLocalizedWord("field_cannot_be_unknown", lang));
+		}
+
+		return ve;
+	}
+}
