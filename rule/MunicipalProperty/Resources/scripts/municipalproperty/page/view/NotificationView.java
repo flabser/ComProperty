02db578564d4ecae303454693bc36758ef84b64b
JohnFlabs <john_198708@mail.ru>
2016-03-28 15:04:23 +0600

    commit

---
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/NotificationView.java
@@ -0,0 +1,14 @@
+package municipalproperty.page.view;
+
+import kz.lof.scripting._Session;
+import kz.lof.scripting._WebFormData;
+
+
+
+public class NotificationView extends AbstractMunicipalPropertyView {
+
+    @Override
+    public void doGET(_Session session, _WebFormData formData) {
+
+    }
+}
