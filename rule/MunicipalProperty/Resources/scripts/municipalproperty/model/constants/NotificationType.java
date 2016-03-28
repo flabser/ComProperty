f038efbcc0b12e6dfd390136395a37b81d29dfdf
k-zon_000 <k-zon_000@KayraNote>
2016-03-28 09:08:44 +0300

    Notification entity was added

---
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/model/constants/NotificationType.java
@@ -0,0 +1,24 @@
+package municipalproperty.model.constants;
+
+public enum NotificationType {
+	UNKNOWN(0), OBJECT_REGISTERED(650), CONTRACT_EXPIRED(651), ORDER_REGISTERED(652), CONTRACT_REGISTERD(653), OBJECT_LOADED(654);
+
+	private int code;
+
+	NotificationType(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public static NotificationType getType(int code) {
+		for (NotificationType type : values()) {
+			if (type.code == code) {
+				return type;
+			}
+		}
+		return UNKNOWN;
+	}
+}
