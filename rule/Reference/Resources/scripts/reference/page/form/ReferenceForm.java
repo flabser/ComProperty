3daea0fb4678e52be6726bffcb9cc6cbe807b8bc
k-zon_000 <k-zon_000@KayraNote>
2016-03-29 19:38:21 +0300

    w

--- rule/Reference/Resources/scripts/reference/page/form/ReferenceForm.java
+++ rule/Reference/Resources/scripts/reference/page/form/ReferenceForm.java
@@ -1,17 +1,12 @@
 package reference.page.form;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import kz.lof.administrator.dao.LanguageDAO;
-import kz.lof.administrator.model.Language;
+import kz.lof.common.model.page.form.Form;
 import kz.lof.localization.LanguageCode;
 import kz.lof.scripting._Session;
 import kz.lof.scripting._Validation;
 import kz.lof.scripting._WebFormData;
-import kz.lof.scripting.event._DoPage;
 import kz.lof.scriptprocessor.page.IOutcomeObject;
 import kz.lof.user.IUser;
 import kz.lof.user.SuperUser;
@@ -24,7 +19,7 @@ import reference.model.Reference;
  * @author Kayra created 03-01-2016
  */
 
-public abstract class ReferenceForm extends _DoPage {
+public abstract class ReferenceForm extends Form {
 
 	protected _Validation validate(_WebFormData formData, LanguageCode lang) {
 		_Validation ve = new _Validation();
@@ -52,20 +47,6 @@ public abstract class ReferenceForm extends _DoPage {
 		entity.setRegDate(new Date());
 		entity.setName("");
 		return entity;
-	}
-
-	protected Map<LanguageCode, String> getLocalizedNames(_Session session, _WebFormData formData) {
-		Map<LanguageCode, String> localizedNames = new HashMap<LanguageCode, String>();
-		List<Language> langs = new LanguageDAO(session).findAll();
-		for (Language l : langs) {
-			String ln = formData.getValueSilently(l.getCode().name().toLowerCase() + "localizedname");
-			if (!ln.isEmpty()) {
-				localizedNames.put(l.getCode(), ln);
-			} else {
-				localizedNames.put(l.getCode(), formData.getValueSilently("name"));
-			}
-		}
-		return localizedNames;
 	}
 
 	@Override
