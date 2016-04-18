75eb09e4c3271dea39e060087da69ce6961fc699
k-zon_000 <k-zon_000@KayraNote>
2016-04-18 23:03:32 +0300

    f

--- rule/GisServProvider/Resources/scripts/gisservprovider/page/view/InfoView.java
+++ rule/GisServProvider/Resources/scripts/gisservprovider/page/view/InfoView.java
@@ -1,13 +1,8 @@
 package gisservprovider.page.view;
 
-import com.exponentus.dataengine.jpa.ViewPage;
-import com.exponentus.scripting._POJOListWrapper;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.event._DoPage;
-
-import municipalproperty.dao.RealEstateDAO;
-import municipalproperty.model.RealEstate;
 
 /**
  * @author Kayra created 18-04-2016
@@ -22,8 +17,6 @@ public class InfoView extends _DoPage {
 		if (formData.containsField("page")) {
 			pageNum = formData.getNumberValueSilently("page", pageNum);
 		}
-		RealEstateDAO nDao = new RealEstateDAO(session);
-		ViewPage<RealEstate> vp = nDao.findAllInconsistAddr(pageNum, pageSize);
-		addContent(new _POJOListWrapper(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum(), session));
+
 	}
 }
