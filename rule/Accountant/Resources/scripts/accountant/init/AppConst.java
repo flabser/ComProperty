ec40c754b082ac1fdd1358513af0be41798560bd
k-zon_000 <k-zon_000@KayraNote>
2016-04-26 09:08:15 +0300

    1

--- rule/Accountant/Resources/scripts/accountant/init/AppConst.java
+++ rule/Accountant/Resources/scripts/accountant/init/AppConst.java
@@ -4,7 +4,7 @@ public class AppConst {
 	public static String NAME = "Accountant";
 	public static String NAME_ENG = "Accountant";
 	public static String NAME_RUS = "Импорт объектов";
-	public static String NAME_KZ = "Нысан импорт";
+	public static String NAME_KAZ = "Нысан импорт";
 	public static String DEFAULT_URL = "p?id=upload-updating-form";
 	public static String FT_INDEX_SCOPE = "[{\"tableName\":\"properties\",\"fieldNames\":"
 	        + "[\"object_name\",\"description\",\"notes\",\"inv_number\"],\"daoImpl\":\"municipalproperty.dao.PropertyDAO\"}]";
--- rule/GisServProvider/Resources/scripts/gisservprovider/init/AppConst.java
+++ rule/GisServProvider/Resources/scripts/gisservprovider/init/AppConst.java
@@ -4,7 +4,7 @@ public class AppConst {
 	public static String NAME = "GisServProvider";
 	public static String NAME_ENG = "Service Provider for GIS";
 	public static String NAME_RUS = "Сервис для ГИС";
-	public static String NAME_KZ = "Сервис для ГИС";
+	public static String NAME_KAZ = "Сервис для ГИС";
 	public static String DEFAULT_URL = "p?id=info-form";
 	public static String FT_INDEX_SCOPE = "";
 }
