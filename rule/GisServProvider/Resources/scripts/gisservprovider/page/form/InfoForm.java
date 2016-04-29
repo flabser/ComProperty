87716b858aa13c4303e5717bc4cf1ec1e479f231
medin84 <kalihanovm@gmail.com>
2016-04-29 14:29:26 +0600

    commit

--- rule/GisServProvider/Resources/scripts/gisservprovider/page/form/InfoForm.java
+++ rule/GisServProvider/Resources/scripts/gisservprovider/page/form/InfoForm.java
@@ -6,23 +6,16 @@ import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.actions._Action;
 import com.exponentus.scripting.actions._ActionBar;
 import com.exponentus.scripting.actions._ActionType;
-
 import municipalproperty.page.form.AbstractMunicipalPropertyForm;
 
 public class InfoForm extends AbstractMunicipalPropertyForm {
 
-	@Override
-	public void doGET(_Session session, _WebFormData formData) {
-		addValue("getbycoord", Environment.getFullHostName() + "/" + session.getAppEnv().appName + "/rest/gis/getbycoord/0");
-		addValue("getbystreet", Environment.getFullHostName() + "/" + session.getAppEnv().appName + "/rest/gis/getbystreet/0/0");
-		_ActionBar actionBar = new _ActionBar(session);
-		actionBar.addAction(new _Action("Close", "just close the form", _ActionType.CLOSE));
-		addContent(actionBar);
-	}
-
-	@Override
-	public void doPOST(_Session session, _WebFormData formData) {
-
-	}
-
+    @Override
+    public void doGET(_Session session, _WebFormData formData) {
+        addValue("getbycoord", "/" + session.getAppEnv().appName + "/rest/gis/getbycoord/0");
+        addValue("getbystreet", "/" + session.getAppEnv().appName + "/rest/gis/getbystreet/0/0");
+        _ActionBar actionBar = new _ActionBar(session);
+        actionBar.addAction(new _Action("Close", "just close the form", _ActionType.CLOSE));
+        addContent(actionBar);
+    }
 }
