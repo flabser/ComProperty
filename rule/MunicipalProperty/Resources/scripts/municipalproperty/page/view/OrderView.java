e4a97e3925e5236fdeefcd71ecb8d565e18f6612
k-zon_000 <k-zon_000@KayraNote>
2016-05-05 10:51:00 +0300

    banned the order button

--- rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/OrderView.java
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/page/view/OrderView.java
@@ -1,10 +1,10 @@
 package municipalproperty.page.view;
 
 import com.exponentus.dataengine.jpa.ViewPage;
-import com.exponentus.localization.LanguageCode;
 import com.exponentus.scripting._POJOListWrapper;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
+
 import municipalproperty.dao.OrderDAO;
 import municipalproperty.dao.PropertyDAO;
 import municipalproperty.model.Order;
@@ -16,22 +16,22 @@ import municipalproperty.model.Property;
 
 public class OrderView extends AbstractMunicipalPropertyView {
 
-    @Override
-    public void doGET(_Session session, _WebFormData formData) {
-        String propertyId = formData.getValueSilently("propertyid");
-        LanguageCode lang = session.getLang();
-        OrderDAO orderDAO = new OrderDAO(session);
+	@Override
+	public void doGET(_Session session, _WebFormData formData) {
+		String propertyId = formData.getValueSilently("propertyid");
+		session.getLang();
+		OrderDAO orderDAO = new OrderDAO(session);
 
-        if (!propertyId.isEmpty()) {
-            PropertyDAO propertyDAO = new PropertyDAO(session);
-            Property property = propertyDAO.findById(propertyId);
-            if (property != null) {
-                ViewPage<Order> result = orderDAO.findAllByProperty(property);
-                addContent(new _POJOListWrapper<>(result.getResult(), result.getMaxPage(), result.getCount(), result.getPageNum(), session));
-            }
-        } else {
-            addContent(getSimpleActionBar(session, "order-form", lang));
-            addContent(getViewPage(orderDAO, formData));
-        }
-    }
+		if (!propertyId.isEmpty()) {
+			PropertyDAO propertyDAO = new PropertyDAO(session);
+			Property property = propertyDAO.findById(propertyId);
+			if (property != null) {
+				ViewPage<Order> result = orderDAO.findAllByProperty(property);
+				addContent(new _POJOListWrapper<>(result.getResult(), result.getMaxPage(), result.getCount(), result.getPageNum(), session));
+			}
+		} else {
+			// addContent(getSimpleActionBar(session, "order-form", lang));
+			addContent(getViewPage(orderDAO, formData));
+		}
+	}
 }
