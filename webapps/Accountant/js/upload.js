c75719501aeb192307e7355466245648590db6fd
John <john_198708@mail.ru>
2016-05-20 13:51:42 +0600

    commit

--- webapps/Accountant/js/upload.js
+++ webapps/Accountant/js/upload.js
@@ -183,8 +183,6 @@ function renderFilePanel(fileName, fsid) {
     if(status == 1){
         $('.js-check').attr("disabled", false);
     }
-    
-
     $tpl.find('.js-sign').on('click', function(e) {
         e.stopPropagation();
         e.preventDefault();
