d5e4eab6073d11990334f3032239869b35e7bdb2
kaira <biosubj@gmail.com>
2017-04-11 16:26:48 +0300

    #

--- rule/MunicipalProperty/Resources/scripts/municipalproperty/page/report/ReportUtil.java
+++ rule/MunicipalProperty/Resources/scripts/municipalproperty/page/report/ReportUtil.java
@@ -9,9 +9,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-import com.exponentus.dataengine.DatabaseUtil;
 import com.exponentus.dataengine.IDBConnectionPool;
 import com.exponentus.dataengine.IDatabase;
+import com.exponentus.dataengine.jdbc.DatabaseUtil;
+import com.exponentus.env.Environment;
 import com.exponentus.scripting._Session;
 import com.exponentus.server.Server;
 
@@ -21,33 +22,35 @@ public class ReportUtil {
 
 	public static HashMap<String, List<KufType>> getCat() {
 		HashMap<String, List<KufType>> cat = new HashMap<String, List<KufType>>();
-		ArrayList<KufType> peList = new ArrayList<KufType>(
-		        Arrays.asList(KufType.FURNITURE, KufType.ANIMALS, KufType.SPORT_EQUIPMENT, KufType.OTHERS, KufType.SHARE_BLOCK, KufType.EQUITY));
+		ArrayList<KufType> peList = new ArrayList<KufType>(Arrays.asList(KufType.FURNITURE, KufType.ANIMALS,
+				KufType.SPORT_EQUIPMENT, KufType.OTHERS, KufType.SHARE_BLOCK, KufType.EQUITY));
 		cat.put("personalstateCat", peList);
 		cat.put("personalestate_report", peList);
 
-		ArrayList<KufType> eList = new ArrayList<KufType>(Arrays.asList(KufType.SCHOOL_EQUIPMENT, KufType.OFFICE_EQUIPMENT,
-		        KufType.COMPUTER_EQUIPMENT, KufType.MEDICAL_EQUIPMENT, KufType.COOK_EQUIPMENT, KufType.EQUIPMENT_OF_CIVIL_DEFENCE));
+		ArrayList<KufType> eList = new ArrayList<KufType>(
+				Arrays.asList(KufType.SCHOOL_EQUIPMENT, KufType.OFFICE_EQUIPMENT, KufType.COMPUTER_EQUIPMENT,
+						KufType.MEDICAL_EQUIPMENT, KufType.COOK_EQUIPMENT, KufType.EQUIPMENT_OF_CIVIL_DEFENCE));
 		cat.put("equipmentCat", eList);
 		cat.put("equipment_report", eList);
 
-		ArrayList<KufType> reList = new ArrayList<KufType>(
-		        Arrays.asList(KufType.BUILDINGS, KufType.ROOMS, KufType.STRUCTURES, KufType.RESIDENTIAL_OBJECTS, KufType.LAND, KufType.MONUMENT));
+		ArrayList<KufType> reList = new ArrayList<KufType>(Arrays.asList(KufType.BUILDINGS, KufType.ROOMS,
+				KufType.STRUCTURES, KufType.RESIDENTIAL_OBJECTS, KufType.LAND, KufType.MONUMENT));
 		cat.put("realestateCat", reList);
 		cat.put("realestate_report", reList);
 
-		ArrayList<KufType> tList = new ArrayList<KufType>(Arrays.asList(KufType.CARGO, KufType.BUS, KufType.TROLLEYBUS, KufType.TRAM,
-		        KufType.WATER_TRANSPORT, KufType.HOSPITAL_TRANSPORT, KufType.SPECIAL_EQUIPMENT));
+		ArrayList<KufType> tList = new ArrayList<KufType>(Arrays.asList(KufType.CARGO, KufType.BUS, KufType.TROLLEYBUS,
+				KufType.TRAM, KufType.WATER_TRANSPORT, KufType.HOSPITAL_TRANSPORT, KufType.SPECIAL_EQUIPMENT));
 		cat.put("transportCat", tList);
 		cat.put("transport_report", tList);
 
-		ArrayList<KufType> eiList = new ArrayList<KufType>(Arrays.asList(KufType.BILLBOARD, KufType.COLUMNS, KufType.ELECTRIC_NETWORKS,
-		        KufType.THERMAL_NETWORKS, KufType.GAS, KufType.WATER_SYSTEM, KufType.DRAIN, KufType.ROAD, KufType.PARKING));
+		ArrayList<KufType> eiList = new ArrayList<KufType>(
+				Arrays.asList(KufType.BILLBOARD, KufType.COLUMNS, KufType.ELECTRIC_NETWORKS, KufType.THERMAL_NETWORKS,
+						KufType.GAS, KufType.WATER_SYSTEM, KufType.DRAIN, KufType.ROAD, KufType.PARKING));
 		cat.put("specialconstructionsCat", eiList);
 		cat.put("engineeringInfrastructure_report", eiList);
 
-		ArrayList<KufType> soList = new ArrayList<KufType>(
-		        Arrays.asList(KufType.BOMBPROOF, KufType.FACTORY, KufType.COMBINES, KufType.AIRPORT, KufType.LAND, KufType.TRANSITIONS));
+		ArrayList<KufType> soList = new ArrayList<KufType>(Arrays.asList(KufType.BOMBPROOF, KufType.FACTORY,
+				KufType.COMBINES, KufType.AIRPORT, KufType.LAND, KufType.TRANSITIONS));
 		cat.put("strategicobjectsCat", soList);
 		cat.put("strategicobjects_report", soList);
 
@@ -64,8 +67,8 @@ public class ReportUtil {
 		cat.put("personalstateCat", personalSForm);
 		cat.put("personalestate_report", personalSForm);
 
-		String[] equipmentForms = { "schoolequipment", "officeequipment", "computerequipment", "medicalequipment", "cookequipment",
-		        "equipmentofcivildefense" };
+		String[] equipmentForms = { "schoolequipment", "officeequipment", "computerequipment", "medicalequipment",
+				"cookequipment", "equipmentofcivildefense" };
 		cat.put("equipmentCat", equipmentForms);
 		cat.put("equipment_report", equipmentForms);
 
@@ -73,13 +76,13 @@ public class ReportUtil {
 		cat.put("realestateCat", realSForm);
 		cat.put("realestate_report", realSForm);
 
-		String[] transportForms = { "automobile", "cargo", "bus", "trolleybus", "tram", "watertransport", "hospitaltransport", "specialequipment",
-		        "motorcycle" };
+		String[] transportForms = { "automobile", "cargo", "bus", "trolleybus", "tram", "watertransport",
+				"hospitaltransport", "specialequipment", "motorcycle" };
 		cat.put("transportCat", transportForms);
 		cat.put("transport_report", transportForms);
 
-		String[] engInfStructForms = { "billboard", "columns", "electricnetworks", "thermalnetworks", "gas", "watersystem", "drain", "road",
-		        "parking" };
+		String[] engInfStructForms = { "billboard", "columns", "electricnetworks", "thermalnetworks", "gas",
+				"watersystem", "drain", "road", "parking" };
 		cat.put("specialconstructionsCat", engInfStructForms);
 		cat.put("engineeringInfrastructure_report", engInfStructForms);
 
@@ -94,7 +97,7 @@ public class ReportUtil {
 	}
 
 	public static String getOrgName(_Session ses, int code) {
-		IDatabase db = ses.getDatabase();
+		IDatabase db = Environment.database;
 
 		IDBConnectionPool dbPool = db.getConnectionPool();
 		Connection conn = dbPool.getConnection();
@@ -104,7 +107,7 @@ public class ReportUtil {
 			Statement s = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
 
 			String sql = "select m.viewtext from maindocs as m where ( m.form='kgp' or m.form='kgu' or m.form='ao' "
-			        + "or m.form='too' or m.form='subsidiaries' ) and m.docid=" + code;
+					+ "or m.form='too' or m.form='subsidiaries' ) and m.docid=" + code;
 			ResultSet rs = s.executeQuery(sql);
 
 			if (rs.next()) {
